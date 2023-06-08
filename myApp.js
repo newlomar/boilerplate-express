e9ffcae635dadde8f7ad20790b5d5aeea0928a69
Newton Lomar <newtonlomar16@gmail.com>
2023-06-08 18:55:10 -0300

    dealing with middlewares

--- myApp.js
+++ myApp.js
@@ -2,6 +2,11 @@ let express = require('express');
 let app = express();
 require('dotenv').config()
 
+app.use(function(req, res, next) {
+  console.log(`${req.method} ${req.path} - ${req.ip}`)
+  next()
+})
+
 app.get("/json", function(req, res) {
   const message_style = process.env.MESSAGE_STYLE
   if (message_style === "uppercase") {
