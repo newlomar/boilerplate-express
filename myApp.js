19baceb27f614ab2ea20aba2746347306f5970f4
Newton Lomar <newtonlomar16@gmail.com>
2023-06-08 18:57:44 -0300

    chaining middleware with handler inside specific route

--- myApp.js
+++ myApp.js
@@ -4,7 +4,15 @@ require('dotenv').config()
 
 app.use(function(req, res, next) {
   console.log(`${req.method} ${req.path} - ${req.ip}`)
+
   next()
+})
+
+app.get("/now", function(req, res, next) {
+  req.time = new Date().toString()
+  next()
+}, function(req, res) {
+  res.json({time: req.time})
 })
 
 app.get("/json", function(req, res) {
